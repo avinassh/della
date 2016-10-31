2251bffd41f886d36571b508895af6f1db40dc5d
avi <hi@avi.im>
2016-10-31 18:23:51 +0530

    Add utils

---
+++ della/utils.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class TimeStampMixin(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
