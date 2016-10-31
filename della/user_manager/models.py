f3e43612e974432af550387ed648a7b2061f42c9
avi <hi@avi.im>
2016-10-31 22:19:02 +0530

    Create model UserProfile

--- della/user_manager/models.py
+++ della/user_manager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from della.utils import TimeStampMixin
+
+
+class UserProfile(TimeStampMixin):
+    """
+    To manage users and stuff. From default `User` we will use `username`,
+    `email`, `password`, `first_name` and `last_name`.
+    """
+    bio = models.TextField(null=True)
+    address = models.TextField(null=True)
+    shipping_instructions = models.TextField(null=True)
+    is_enabled_exchange = models.BooleanField(default=False)
+    preferences = models.TextField(null=True)
+    fb_profile_url = models.URLField(null=True)
+    twitter_profile_url = models.URLField(null=True)
+    website_url = models.URLField(null=True)
+    wishlist_url = models.URLField(null=True)
+
+    user = models.OneToOneField(User)
