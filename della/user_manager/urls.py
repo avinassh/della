683a9ca0224eec001c365aa7f6277194d25d2b6f
avi <hi@avi.im>
2016-11-01 10:31:16 +0530

    Use default views and add login/logout

--- della/user_manager/urls.py
+++ della/user_manager/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from .views import SignupView
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login',
+        kwargs={'template_name': 'user_manager/login.html'}),
+    url(r'^logout/$', auth_views.logout, name='logout',
+        kwargs={'next_page': '/'}),
     url(r'^create/$', SignupView.as_view()),
 ]
