dd4a33a97f2074b39b73ceb0a936b2646800def8
avi <hi@avi.im>
2016-11-01 17:28:16 +0530

    Add title and description fields for `Image`

--- della/gallery/models.py
+++ della/gallery/models.py
@@ -6,4 +6,6 @@ from della.utils import TimeStampMixin
 
 class Image(TimeStampMixin):
     file = models.ImageField(upload_to='images/%Y/%m/%d')
+    title = models.CharField(max_length=300)
+    description = models.TextField(null=True)
     added_by = models.ForeignKey(User)
