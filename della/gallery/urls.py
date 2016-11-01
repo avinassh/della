9c8028cdc50844d0f224337270ac4c6bd29b0154
avi <hi@avi.im>
2016-11-01 20:00:58 +0530

    Redirect to uploaded image page once image is added

--- della/gallery/urls.py
+++ della/gallery/urls.py
@@ -4,5 +4,5 @@ from .views import ImageUploadView, ImageDetailView
 
 urlpatterns = [
     url(r'^upload/$', ImageUploadView.as_view()),
-    url(r'^images/(?P<pk>\d+)/$', ImageDetailView.as_view())
+    url(r'^images/(?P<pk>\d+)/$', ImageDetailView.as_view(), name='image-detail')
 ]
--- della/gallery/views.py
+++ della/gallery/views.py
@@ -2,6 +2,7 @@ from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.shortcuts import reverse
 
 from .models import Image
 
@@ -9,13 +10,15 @@ from .models import Image
 @method_decorator(login_required, name='dispatch')
 class ImageUploadView(CreateView):
     model = Image
-    success_url = '/'
     fields = ['file', 'title', 'description']
 
     def form_valid(self, form):
         image = form.save(commit=False)
         image.added_by = self.request.user
         return super(ImageUploadView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('gallery:image-detail', args=(self.object.id,))
 
 
 class ImageDetailView(DetailView):
