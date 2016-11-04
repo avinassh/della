28478db4b317e35ecbb63e5c2b1f4a7724b256d9
avi <hi@avi.im>
2016-11-04 17:19:13 +0530

    Use `@` in message thread

--- della/inbox/urls.py
+++ della/inbox/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import MessageCreateView, ThreadDetailView, ThreadListView
 
 urlpatterns = [
-    url(r'^(?P<recipient>[a-zA-Z0-9_]+)/$', ThreadDetailView.as_view(),
+    url(r'^@(?P<recipient>[a-zA-Z0-9_]+)/$', ThreadDetailView.as_view(),
         name='thread-detail'),
     url(r'^(?P<pk>\d+)/new/$', MessageCreateView.as_view(),
         name='new-message'),
