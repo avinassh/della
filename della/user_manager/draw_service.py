e563e8f8f1af691c4c9aa2f6177fbf2c8e2a4855
avi <hi@avi.im>
2016-11-05 18:53:34 +0530

    Add helper funtions for making pairs

--- della/user_manager/draw_service.py
+++ della/user_manager/draw_service.py
@@ -1,6 +1,9 @@
 import json
+import random
+from collections import deque
 
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 def _get_default_file_content():
@@ -33,4 +36,35 @@ def get_draw_status():
 def flip_draw_status():
     if not _get_status_file()['status']:
         return _write_status_file()
+    return True
+
+
+def draw_names():
+    pass
+
+
+def make_pairs(user_ids):
+    while True:
+        pairs = _get_pairs(user_ids=user_ids)
+        if _is_valid_pair(pairs=pairs):
+            break
+    return pairs
+
+
+def _get_pairs(user_ids):
+    user_ids_copy = user_ids.copy()
+    random.shuffle(user_ids_copy)
+    pairs = deque(user_ids_copy)
+    pairs.rotate()
+    return list(zip(user_ids, user_ids_copy))
+
+
+def _is_valid_pair(pairs):
+    """
+    Checks if the pair and list of pairs is valid. A pair is invalid if both
+    santa and santee are same i.e. (1, 1)
+    """
+    for pair in pairs:
+        if pair[0] == pair[1]:
+            return False
     return True
