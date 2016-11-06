75b7160fa4d110257ac4d785ffbe7416d1e22000
avi <hi@avi.im>
2016-11-06 23:01:51 +0530

    Make `recipient_list` required parameter in `send_email`

--- della/user_manager/email_service.py
+++ della/user_manager/email_service.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 
-def send_email(subject, message, recipient_list=()):
+def send_email(subject, message, recipient_list):
     send_mail(
         subject=subject,
         message=message,
