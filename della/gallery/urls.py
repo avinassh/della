4a977a3ab4ed1f078e93b8e023d098e403f0b2d0
avi <hi@avi.im>
2016-11-08 17:54:35 +0530

    Add image upload button on gallery

--- della/gallery/urls.py
+++ della/gallery/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import ImageUploadView, ImageDetailView, ImageListView
 
 urlpatterns = [
-    url(r'^upload/$', ImageUploadView.as_view()),
+    url(r'^upload/$', ImageUploadView.as_view(), name='upload'),
     url(r'^(?P<pk>\d+)/$', ImageDetailView.as_view(), name='image-detail'),
     url(r'^$', ImageListView.as_view(), name='image-list')
 ]
