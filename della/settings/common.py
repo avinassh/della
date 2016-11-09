d8926a19718aa1447843d5ae3fb3757b92a4c5cf
avi <hi@avi.im>
2016-11-09 19:59:12 +0530

    Override default messages tags with custom ones

--- della/settings/common.py
+++ della/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,3 +144,11 @@ EMAIL_LINK_EXPIRY_DAYS = 2 * 86400
 # Django Crispy forms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 CRISPY_FAIL_SILENTLY = not DEBUG
+
+# override default message tags so that they work nicely with bootstrap
+# error classes
+MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
+                message_constants.INFO: 'info',
+                message_constants.SUCCESS: 'success',
+                message_constants.WARNING: 'warning',
+                message_constants.ERROR: 'danger'}
