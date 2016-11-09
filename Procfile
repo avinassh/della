c62dc81588c4b20985df60a8455d9bfd93bf95a3
avi <hi@avi.im>
2016-11-09 22:32:54 +0530

    Add settings.heroku and files required for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nightreads.wsgi --log-file -
---
+++ della/settings/heroku.py
@@ -0,0 +1,21 @@
+import dj_database_url
+import os
+
+from .common import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+INVITE_CODE = os.environ['INVITE_CODE']
+SENDER_EMAIL = os.environ['SENDER_EMAIL']
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
