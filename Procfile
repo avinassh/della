b0dce405b6f6baaf394d846472966b673e38e85e
avi <hi@avi.im>
2016-11-09 23:04:33 +0530

    Fix `wsgi` settings for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn nightreads.wsgi --log-file -
+web: gunicorn della.wsgi --log-file -
--- della/wsgi.py
+++ della/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for della project.
+WSGI config for nightreads project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "della.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "della.settings.heroku")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
