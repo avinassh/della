8c18c33d3faad78ba1fc89b04028f83b7f33cf79
avi <hi@avi.im>
2016-11-22 13:23:36 +0530

    Add sample secret file

---
+++ della/settings/sample_secret.py
@@ -0,0 +1,19 @@
+# DB Settings
+DB_SETTINGS = {
+    'NAME': 'DB_NAME',
+    'USER': 'DB_USERNAME',
+    'PASSWORD': 'DB_PASSWORD',
+    'HOST': 'DB_HOST',
+    'PORT': 'DB_PORT'
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+INVITE_CODE = 'HAHAHAHAHAHA'
+SENDER_EMAIL = 'admin@secret.santa'
+
+ADMINS = (
+    ('v', 'v@secret.santa'),
+    ('avinassh', 'avinash@secret.santa')
+)
