76a59fc8582d33a526820f13cfd50c162b14446f
avi <hi@avi.im>
2016-11-28 19:56:33 +0530

    Update homepage to display calender and santee details

--- della/views.py
+++ della/views.py
@@ -6,8 +6,12 @@ from della.user_manager.forms import SignupForm
 class HomePageView(TemplateView):
 
     template_name = 'home.html'
+    template_name_authenticated = 'home_authenticated.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated():
+            self.template_name = self.template_name_authenticated
+            return context
         context['form'] = SignupForm()
         return context
